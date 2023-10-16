641ec76623f59d973c1c72cd3721a72f3be17a5f
Mahib22 <mahib.arib@gmail.com>
2023-10-16 20:27:04 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ProductDisplay from './components/ProductDisplay.vue'
+</script>
+
+<template>
+  <main class="bg-gray-200 min-h-screen py-24">
+    <ProductDisplay />
+  </main>
+</template>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="w-3/4 bg-white mx-auto rounded-lg shadow-md">
+    <div class="flex flex-col md:flex-row px-6 py-8 items-center">
+      <div class="md:w-1/3 md:px-6">
+        <img
+          alt="content"
+          class="object-cover object-center h-full w-full"
+          src="https://dummyimage.com/1200x1600"
+        />
+      </div>
+
+      <div class="md:w-2/3 md:px-8 pt-4 md:pt-0">
+        <h1 class="font-semibold text-2xl mb-4">
+          Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket
+        </h1>
+
+        <div class="flex justify-between items-center border-b pb-1">
+          <p>women's clothing</p>
+          <div class="flex gap-2">
+            <p>2.9/5</p>
+            <div class="flex gap-1">
+              <img src="../assets/icon/ellipse-blue.svg" />
+              <img src="../assets/icon/ellipse-blue.svg" />
+              <img src="../assets/icon/ellipse-blue.svg" />
+              <img src="../assets/icon/ellipse-blue.svg" />
+              <img src="../assets/icon/ellipse-blue.svg" />
+            </div>
+          </div>
+        </div>
+
+        <div class="border-b pt-5 pb-10">
+          <p>
+            100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux
+            leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim
+            style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH
+            ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON
+          </p>
+        </div>
+
+        <h2 class="py-2 font-semibold">$29.95</h2>
+
+        <div class="flex gap-4">
+          <button class="w-1/2 rounded py-1 bg-gray-600 text-white font-semibold">Buy now</button>
+          <button class="w-1/2 rounded py-1 border-2 border-gray-500 font-semibold text-black">
+            Next product
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
