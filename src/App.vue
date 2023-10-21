b84c443b615c6765b0ba5fa0910fa20be4a2c53c
Mahib22 <mahib.arib@gmail.com>
2023-10-21 10:33:11 +0700

    fetch api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,62 @@
 <script setup>
+import { ref, watchEffect } from 'vue'
 import ProductDisplay from './components/ProductDisplay.vue'
+import ProductUnavailable from './components/ProductUnavailable.vue'
+
+const product = ref([])
+const productId = ref(1)
+const isLoading = ref(false)
+const isUnavailable = ref(false)
+
+const incrementProductId = () => {
+  productId.value > 19 ? (productId.value = 1) : productId.value++
+}
+
+watchEffect(async () => {
+  isLoading.value = true
+
+  try {
+    const response = await fetch(`https://fakestoreapi.com/products/${productId.value}`)
+    let data = await response.json()
+
+    if (data.category == "men's clothing" || data.category == "women's clothing") {
+      isUnavailable.value = false
+      product.value = data
+    } else {
+      isUnavailable.value = true
+    }
+
+    isLoading.value = false
+  } catch (error) {
+    alert('Terjadi kesalahan saat mengakses API')
+  }
+})
 </script>
 
 <template>
   <main class="bg-gray-200 min-h-screen py-24">
-    <ProductDisplay />
+    <div class="w-3/4 bg-white mx-auto rounded-lg shadow-md">
+      <ProductUnavailable v-if="isUnavailable">
+        <template #nextButton>
+          <button
+            class="w-2/5 rounded py-1 border-2 border-gray-600 font-semibold"
+            @click="incrementProductId"
+          >
+            Next product
+          </button>
+        </template>
+      </ProductUnavailable>
+
+      <ProductDisplay :product="product" :isLoading="isLoading" v-else>
+        <template #nextButton>
+          <button
+            class="w-1/2 rounded py-1 border-2 border-gray-600 font-semibold"
+            @click="incrementProductId"
+          >
+            Next product
+          </button>
+        </template>
+      </ProductDisplay>
+    </div>
   </main>
 </template>
--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,50 +1,74 @@
+<script setup>
+defineProps({
+  product: Object,
+  isLoading: Boolean
+})
+</script>
+
 <template>
-  <div class="w-3/4 bg-white mx-auto rounded-lg shadow-md">
-    <div class="flex flex-col md:flex-row px-6 py-8 items-center">
-      <div class="md:w-1/3 md:px-6">
-        <img
-          alt="content"
-          class="object-cover object-center h-full w-full"
-          src="https://dummyimage.com/1200x1600"
-        />
-      </div>
+  <div class="flex flex-col md:flex-row px-6 py-8 items-center">
+    <div class="md:w-1/3 md:px-6">
+      <img
+        :class="[
+          isLoading
+            ? 'animate-pulse bg-slate-200 rounded-lg h-60'
+            : 'object-cover object-center h-full w-full'
+        ]"
+        :src="isLoading ? '' : product.image"
+      />
+    </div>
 
-      <div class="md:w-2/3 md:px-8 pt-4 md:pt-0">
-        <h1 class="font-semibold text-2xl mb-4">
-          Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket
-        </h1>
+    <div class="md:w-2/3 md:px-8 pt-4 md:pt-0">
+      <h1
+        :class="[
+          isLoading
+            ? 'bg-slate-200 h-5 w-full my-2 animate-pulse rounded'
+            : 'font-semibold text-2xl mb-4'
+        ]"
+      >
+        {{ isLoading ? '' : product.title }}
+      </h1>
 
-        <div class="flex justify-between items-center border-b pb-1">
-          <p>women's clothing</p>
-          <div class="flex gap-2">
-            <p>2.9/5</p>
-            <div class="flex gap-1">
-              <img src="../assets/icon/ellipse-blue.svg" />
-              <img src="../assets/icon/ellipse-blue.svg" />
-              <img src="../assets/icon/ellipse-blue.svg" />
-              <img src="../assets/icon/ellipse-blue.svg" />
-              <img src="../assets/icon/ellipse-blue.svg" />
-            </div>
+      <div class="flex justify-between items-center border-b pb-1">
+        <p :class="[isLoading ? 'bg-slate-200 h-5 w-1/5 my-2 animate-pulse rounded' : '']">
+          {{ isLoading ? '' : product.category }}
+        </p>
+
+        <div
+          :class="[isLoading ? 'bg-slate-200 h-5 w-1/3 my-2 animate-pulse rounded' : 'flex gap-2']"
+        >
+          <p>{{ isLoading ? '' : `${product.rating?.rate}/5` }}</p>
+          <div v-if="!isLoading" class="flex gap-1">
+            <button
+              class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"
+            ></button>
+            <button
+              class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"
+            ></button>
+            <button
+              class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"
+            ></button>
           </div>
         </div>
+      </div>
 
-        <div class="border-b pt-5 pb-10">
-          <p>
-            100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux
-            leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim
-            style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH
-            ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON
-          </p>
-        </div>
+      <div class="border-b pt-5 pb-10">
+        <p :class="[isLoading ? 'bg-slate-200 h-5 w-1/3 animate-pulse rounded' : '']">
+          {{ isLoading ? '' : product.description }}
+        </p>
+      </div>
 
-        <h2 class="py-2 font-semibold">$29.95</h2>
+      <h2
+        :class="[
+          isLoading ? 'bg-slate-200 h-5 w-1/3 my-2 animate-pulse rounded' : 'py-2 font-semibold'
+        ]"
+      >
+        {{ isLoading ? '' : `$${product.price}` }}
+      </h2>
 
-        <div class="flex gap-4">
-          <button class="w-1/2 rounded py-1 bg-gray-600 text-white font-semibold">Buy now</button>
-          <button class="w-1/2 rounded py-1 border-2 border-gray-500 font-semibold text-black">
-            Next product
-          </button>
-        </div>
+      <div class="flex gap-4">
+        <button class="w-1/2 rounded py-1 bg-gray-600 text-white font-semibold">Buy now</button>
+        <slot name="nextButton"></slot>
       </div>
     </div>
   </div>
